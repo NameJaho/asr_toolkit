13d53be351dca0be720404c33b1b45779cc82231
liujeru <jeru.token@gmail.com>
2024-03-15 17:55:39 +0800

    add wav detector

--- wav_detector.py
+++ wav_detector.py
@@ -2,11 +2,16 @@ import numpy as np
 import librosa
 import librosa.display
 from funasr import AutoModel
+import tools.my_utils as utils
+import os
+
+VAD_MODEL_NAME = "speech_fsmn_vad_zh-cn-16k-common-pytorch"
 
 
 class WavDetector:
     def __init__(self, audio_path):
         self.audio_path = audio_path
+        self.vad_model_path = os.path.join(utils.get_root_path(), "models", VAD_MODEL_NAME)
 
     def load_audio(self):
         y, sr = librosa.load(self.audio_path)
@@ -24,7 +29,7 @@ class WavDetector:
         return length_in_seconds
 
     def check_vocal(self):
-        model = AutoModel(model="fsmn-vad")
+        model = AutoModel(model=self.vad_model_path)
 
         total_length = self.get_audio_length()
         vocal_chunks = model.generate(input=self.audio_path)
@@ -39,12 +44,33 @@ class WavDetector:
         return gaps, vocal_pct
 
     def extract_features(self):
-        y, sr = self.load_audio()
+        wav, freq = self.load_audio()
 
         features = dict()
-        features['wav_freq'] = sr
+
+        features['wav_freq'] = freq
+        features['wav_points'] = wav.shape[0]
+        features['tempo_bpm'] = librosa.beat.beat_track(y=wav, sr=freq)[0]
+
+        beat_frames = librosa.beat.beat_track(y=wav, sr=freq)[1]
+        beat_times = librosa.frames_to_time(beat_frames, sr=freq)
+        rolloff_freq = np.mean(librosa.feature.spectral_rolloff(y=wav, sr=freq, hop_length=512, roll_percent=0.9))
+
+        features['avg_diff_beat_times'] = round(np.mean(beat_times[1:] - beat_times[0:len(beat_times) - 1]), 4)
+        features['std_diff_beat_times'] = round(np.std(beat_times[1:] - beat_times[0:len(beat_times) - 1]), 4)
+        features['rolloff_freq'] = round(rolloff_freq, 0)
 
         mel_spec = self.calculate_mel_spec()
+        # 时域能量分布特征
+        features['energy_variation'] = np.std(np.sum(mel_spec, axis=0))
+
+        db20_splits = librosa.effects.split(y=wav, frame_length=4000, top_db=20)
+        features['db20_splits_size'] = db20_splits.size
+
+        wav_harm, wav_perc = librosa.effects.hpss(wav)
+
+        features['wav_harm_mean'] = np.mean(wav_harm)
+        features['wav_perc_mean'] = np.mean(wav_perc)
 
         return features
 
