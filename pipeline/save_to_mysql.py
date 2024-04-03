6c95e8495168cce116863b809763d126ce31b18e
Jaho <>
2024-04-03 11:23:26 +0800

    删除文件

--- pipeline/save_to_mysql.py
+++ pipeline/save_to_mysql.py
@@ -54,8 +54,8 @@ def delete_videos(chunk):
             # 如果文件存在，删除文件
             os.remove(filename)
             time.sleep(0.1)
-            if os.path.exists(filename.replace('mp4', 'wav')):
-                os.remove(filename.replace('mp4', 'wav'))
+            if os.path.exists(filename.replace('wav', 'mp4')):
+                os.remove(filename.replace('wav', 'mp4'))
             logger.info(f"文件 '{filename}' 已删除")
         else:
             logger.info(f"文件 '{filename}' 不存在")
